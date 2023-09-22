324a461230ab47ceeb24710f7471fa6f4ae42277
Ian Carlos Bertoncelo <icbertoncelo@MacBook-Pro-de-Ian.local>
2023-09-21 22:38:42 -0300

    feat: add project structure and upload video route

---
+++ src/routes/upload-video.ts
@@ -0,0 +1,50 @@
+import { FastifyInstance } from "fastify";
+import { fastifyMultipart } from '@fastify/multipart'
+import path from "path";
+import {  randomUUID } from "crypto";
+import { promisify } from "util";
+import { pipeline } from "stream";
+import { createWriteStream } from "fs";
+
+import { prisma } from "../lib/prisma";
+
+const pump = promisify(pipeline)
+
+export async function uploadVideoRoute(app: FastifyInstance) {
+  app.register(fastifyMultipart, {
+    limits: {
+      fieldSize: 1048576 * 25 // 25Mb
+    }
+  })
+
+  app.post('/videos', async(request, reply) => {
+    const data = await request.file()
+
+    if(!data) {
+      return reply.status(400).send({ error: "Missing file input." })
+    }
+
+    const fileExtension = path.extname(data.filename)
+
+    if(fileExtension !== ".mp3") {
+      return reply.status(400).send({ error: "Invalid input type. Please upload a MP3." })
+    }
+
+    const fileBaseName = path.basename(data.filename, fileExtension)
+    const fileUploadName = `${fileBaseName}${randomUUID()}${fileExtension}`
+    const uploadDir = path.resolve(__dirname, '..', '..', 'tmp', fileUploadName)
+
+    await pump(data.file, createWriteStream(uploadDir))
+
+    const video = prisma.video.create({
+      data: {
+        name: data.filename,
+        path: uploadDir,
+      }
+    })
+
+    return {
+      video
+    }
+  })
+}
---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import { fastify } from "fastify";
+import { getAllPrompts } from "./routes/get-all-prompts";
+
+const app = fastify()
+
+app.register(getAllPrompts)
+
+app.listen({
+  port: 3333
+}).then(() => {
+  console.log('HTTP Server Running')
+})
